bf020cc185fe27d8660afc63e524b83ca9f8537b
ParkJumyung <parkjumyungpublic@gmail.com>
2024-02-05 16:32:13 +0900

    seperate GPT and ADA deployment key

--- src/utils/vector_DB.py
+++ src/utils/vector_DB.py
@@ -18,7 +18,7 @@ class VectorDB:
 
     def embed(self, texts):
         embeddings = [response["embedding"] for response in self.client.embeddings.create(
-            model=os.getenv("AZURE_OPENAI_DEPLOYMENT"), 
+            model=os.getenv("AZURE_OPENAI_DEPLOYMENT_ADA"), 
             input=texts
         )["data"]]
         return embeddings
