05e635154de42f41fa6a06963e423408addee065
inthree3 <inseon_ug@gm.gist.ac.kr>
2024-02-12 11:50:14 +0900

    feat: add api endpoint to api server

--- src/api/api_server.py
+++ src/api/api_server.py
@@ -14,12 +14,14 @@ from api.query_regenerator.query_regenerator import namespace as query_regenerat
 from api.google_search.google_search import namespace as google_search_namespace
 from api.snippet_extractor.snippet_extractor import namespace as snippet_extractor_namespace
 from api.echo.echo import namespace as echo_namespace
+from api.query_generator.query_generator import namespace as query_generator
 
 api.add_namespace(container_generator_namespace)
 api.add_namespace(query_regenerator_namespace)
 api.add_namespace(google_search_namespace)
 api.add_namespace(snippet_extractor_namespace)
 api.add_namespace(echo_namespace)
+api.add_namespace(query_generator)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
