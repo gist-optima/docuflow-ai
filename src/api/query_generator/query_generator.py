5fabb3b90dd177b71d16e5f99f5e5f1bef23702c
inthree3 <inseon_ug@gm.gist.ac.kr>
2024-02-12 11:49:56 +0900

    feat: implement query_generator for init query generating with valid payload model

---
+++ src/api/query_generator/query_generator.py
@@ -0,0 +1,55 @@
+import os
+import sys
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_PATH)
+
+from utils.gpt import GPT
+import json
+from flask import request, abort
+from flask_restx import Resource, Namespace, fields
+
+current_directory = BASE_PATH + "\\query_generator"
+
+prompt_file_path = os.path.join(current_directory, "prompt.json")
+fewshot_examples_file_path = os.path.join(current_directory, "examples.json")
+
+prompt = {}
+with open(prompt_file_path, "r", encoding="UTF-8") as f:
+    prompt = json.load(f)
+    prompt = prompt["content"]
+
+fewshot_examples = []
+with open(fewshot_examples_file_path, "r", encoding="UTF-8") as f:
+    fewshot_examples = json.load(f)
+    for example in fewshot_examples:
+        example["input"] = json.dumps(example["input"], ensure_ascii=False, indent=4)
+        example["output"] = json.dumps(example["output"], ensure_ascii=False, indent=4)
+
+namespace = Namespace("query-generator")
+
+gpt = GPT(prompt, fewshot_examples)
+
+model=namespace.model('Payload',{
+    'focused container': fields.String(required=True, description="보고서의 전체 내용 중 현재 사용자가 관심있는 부분"),
+    'guiding vector': fields.String(required=True, description="사용자가 원하는 검색의 방향성") # guiding vector may not be required: todo
+})
+
+query_params=[]
+
+class QueryParams:
+    def __init__(self, all_contents, focused_container, guiding_vector):
+        self.all_contents=all_contents
+        self.focused_container=focused_container
+        self.guiding_vector=guiding_vector
+
+@namespace.route('/')
+class QueryGenerator(Resource):
+    @namespace.expect(model)
+    def post(self):
+        body=request.json
+
+        global gpt
+        message = json.dumps(body, ensure_ascii=False)
+        response = gpt.get_response(message)
+        return response["queries"]
+        
