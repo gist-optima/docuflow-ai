95012b6c96eb878e99ae9953377e031dc8209beb
inthree3 <inseon_ug@gm.gist.ac.kr>
2024-02-18 11:05:54 +0900

    fix: class name into modulizer

--- src/api/modulizer/modulizer.py
+++ src/api/modulizer/modulizer.py
@@ -36,12 +36,12 @@ input_schema=namespace.model('Payload',{
 })
 
 @namespace.route("")
-class QueryRegenerator(Resource):
+class Modulizer(Resource):
     @namespace.expect(input_schema)
     def post(self):
         body = request.json
 
         global gpt
         message = json.dumps(body, ensure_ascii=False, indent=4)
-        response = gpt.get_response(message, confinement=False)
+        response = gpt.get_response(message)
         return response
